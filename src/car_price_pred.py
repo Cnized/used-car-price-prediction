b8fbe44b559803d8a07a10a55657eb48b3b5d79f
Cnized <kingkk8181@gmail.com>
2026-08-04 14:40:01 +0330

    Main code updated - Final - Bugs fixed

--- src/car_price_pred.py
+++ src/car_price_pred.py
@@ -12,25 +12,30 @@ from sklearn.ensemble import RandomForestRegressor
 data = pd.read_csv('data/used_cars.csv')
 data['price'] = data['price'].replace('[\\$,]', '', regex=True).astype(float)
 data['milage'] = data['milage'].replace('[^0-9]', '', regex=True).astype(float)
+data = data[data['price'] < 130000]
+
 data['horsepower'] = data['engine'].str.extract(r'(\d+\.?\d*)\s*HP', expand=False).astype(float)
 data['engine_size'] = data['engine'].str.extract(r'(\d+\.\d+)\s*L(?:iter)?', expand=False).astype(float)
-data['model_year'] = data['model_year'].astype(int)
+data['age'] = data['model_year'].max() - data['model_year']
+data = data.drop(columns=['engine'])
+data['horsepower_missing'] = data['horsepower'].isna().astype(int)
+data['engine_size_missing'] = data['engine_size'].isna().astype(int)
+
 
 X = data.drop(['price'], axis=1)
 y = data.price
 
-X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.8, train_size=0.2, random_state= 42)
-
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=42)
 y_train_log = np.log1p(y_train)
 
 num_vals = [c for c in X_train.columns if X_train[c].dtype in ['int64', 'float64']]
-cat_vals = [c for c in X_train.select_dtypes(include=['object', 'string']).columns
-            if X_train[c].nunique() < 50]
+cat_vals = X_train.select_dtypes(include=['object', 'string']).columns.tolist()
 
 
 
 '''transformers for number and categories'''
 num_transformer = SimpleImputer(strategy='constant')
+
 cat_transformer = Pipeline(steps=[
     ('Imputer', SimpleImputer(strategy='most_frequent')),
     ('OneHot', OneHotEncoder(handle_unknown='ignore'))
@@ -46,15 +51,26 @@ preproccessor = ColumnTransformer(transformers=[
 '''Defining our model
 Tune values are obtained by test.
 '''
-model = XGBRegressor(n_estimators= 2700, learning_rate= 0.025, max_depth= 3)
+model = XGBRegressor(
+    objective="reg:squarederror",
+    n_estimators=3000,
+    learning_rate=0.03,
+    max_depth=4,
+    min_child_weight=5,
+    subsample=0.8,
+    colsample_bytree=0.8,
+    reg_lambda=5,
+    reg_alpha=0,
+    random_state=42
+)
 
 final = Pipeline(steps=[
     ('preproccessor', preproccessor),
     ('model', model)
 ])
 
-final.fit(X_train, y_train_log)
-log_pred = final.predict(X_valid)
-pred = np.expm1(log_pred)
+final.fit(X_train, y_train)
+pred = final.predict(X_valid)
+#pred = np.expm1(log_pred)
 
 print('Mean Absolute Error: ',mean_absolute_error(y_valid, pred))
